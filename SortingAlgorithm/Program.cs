64f358a08efb93f5406f23637892349d02631ca6
Matheus daSilva <Matheus.daSilva@INFOTECH.PRI>
2024-02-08 09:28:43 -0700

    Implementing MergeSort

--- SortingAlgorithm/Program.cs
+++ SortingAlgorithm/Program.cs
@@ -16,6 +16,13 @@ internal class Program
         Console.WriteLine("================ Insertion Sort ================");
         InserctionSort inserctionSort = new InserctionSort();
         inserctionSort.sort(new int[10] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 });
+
+        Console.WriteLine("================ Merge Sort ================");
+        MergeSort mergeSort = new MergeSort();
+        //mergeSort.sort(new int[10] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 });
+
+        Console.WriteLine("================ Merge Sort ================");
+        mergeSort.sort(new int[8] { 14,33,27,10,35,19,42,44 });
     }
 
 }
---
+++ SortingAlgorithm/SortingAlgorithms/MergeSort.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithm.SortingAlgorithms
+{
+    internal class MergeSort : Business.SortingAlgorithm
+    {
+        public override void sort(int[] arrayToBeSorted)
+        {
+            printArray(mergeSort(arrayToBeSorted));
+        }
+
+        private int[] mergeSort(int[] arrayToBeSorted)
+        {
+            if (arrayToBeSorted.Length == 1) return null;
+
+            int half = arrayToBeSorted.Length / 2;
+
+            var a = arrayToBeSorted.Take(half).ToArray();
+            var b = arrayToBeSorted.Skip(half).Take(half + 1).ToArray();
+
+            mergeSort(a);
+            mergeSort(b);
+            return merge(a, b);
+        }
+
+        private int[] merge(int[] firstArray, int[] secondArray)
+        {
+            int i = 0;
+            int[] c = new int[firstArray.Length + secondArray.Length];
+
+            while(firstArray.Length > 0 &&  secondArray.Length > 0)
+            {
+                if (firstArray[0] < secondArray[0])
+                {
+                    c[i] = firstArray[0];
+                    firstArray = firstArray.Skip(1).Take(firstArray.Length).ToArray();
+                    i++;
+                } else {
+                    c[i] = secondArray[0];
+                    secondArray = secondArray.Skip(1).Take(secondArray.Length).ToArray();
+                    i++;
+                }
+            }
+
+            while(firstArray.Length > 0)
+            {
+                c[i] = firstArray[0];
+                firstArray = firstArray.Skip(1).Take(firstArray.Length).ToArray();
+                i++;
+            }
+
+            while (secondArray.Length > 0)
+            {
+                c[i] = secondArray[0];
+                secondArray = secondArray.Skip(1).Take(secondArray.Length).ToArray();
+                i++;
+            }
+
+            return c;
+        }
+
+    }
+}
